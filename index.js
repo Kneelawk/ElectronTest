3d02ce8f15eb53db069810998d68b27420a96073
Kneelawk <jedidiah.pommert@gmail.com>
2016-09-20 17:34:05 -0700

    Add partial login functionality

--- index.js
+++ index.js
@@ -3,16 +3,34 @@ const $ = require("./libs/jquery-3.1.0.min.js");
 
 var loggingIn = false;
 
-$(function () {
-  $("[name=curse-login]").click(function () {
+$(() => {
+  $("[name=curse-login]").click(() => {
     if (!loggingIn) {
       loggingIn = true;
       console.log("Logging in...");
-      var username = $("[name=curse-username]").text();
-      var password = $("[name=curse-password]").text();
-      console.log("Username: " + username + ", password: " + password);
+      var username = $("[name=curse-username]").val();
+      var password = $("[name=curse-password]").val();
+      console.log("Username: " + username);
       ipcRenderer.send("login", {username: username, password: password});
       loggingIn = false;
     }
   });
+});
+
+ipcRenderer.on("login-success", (event, arg) => {
+  var result = $("#login-result-field");
+  result.removeClass();
+  result.addClass("login-success-field");
+  console.log("Login success: " + arg.message);
+  result.text(arg.message);
+  result.show();
+});
+
+ipcRenderer.on("login-failure", (event, arg) => {
+  var result = $("#login-result-field");
+  result.removeClass();
+  result.addClass("login-error-field");
+  console.log("Login failure: " + arg.message);
+  result.text(arg.message);
+  result.show();
 });
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow, ipcMain} = require("electron");
+const http = require("http");
 
 let win
 
@@ -15,19 +16,56 @@ function createWindow() {
 
 app.on("ready", createWindow);
 
-app.on("window-all-closed", function () {
+app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("activate", function () {
+app.on("activate", () => {
   if (win == null) {
     createWindow();
   }
 });
 
-ipcMain.on("login", function (event, arg) {
+ipcMain.on("login", (event, arg) => {
   console.log("Logging in...");
-  console.log("Username: " + arg.username + ", password: " + arg.password);
+  console.log("Username: " + arg.username);
+
+  var options = {
+    "host": "curse-rest-proxy.azurewebsites.net",
+    "path": "/authenticate",
+    "method": "POST"
+  };
+  var data = {
+    "username": arg.username,
+    "password": arg.password
+  };
+  var req = http.request(options, (response) => {
+    if (response.statusCode != 200) {
+      var message = "";
+      if (response.statusCode == 401) {
+        message = "401: Bad login";
+      } else {
+        message = response.statusCode;
+      }
+      console.log("Login failure: " + message);
+      event.sender.send("login-failure", { message: "Login failure: " + message})
+    }
+
+    var responseData = "";
+    response.on("data", (data) => {
+      responseData += data;
+    });
+    response.on("finish", (something) => {
+      if (response.statusCode == 200) {
+        var responseObj = JSON.parse(responseData);
+        var message = "Auth token: Token " + responseObj.user_id + ":" + responseObj.token;
+        console.log("Login success: " + message);
+        event.sender.send("login-success", { message: "Login success: " + message });
+      }
+    });
+  });
+  req.write(JSON.stringify(data));
+  req.end();
 });
